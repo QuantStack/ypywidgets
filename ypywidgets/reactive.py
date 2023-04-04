087b4095d20ea3b7c812fe1c2aa0645b5f1d4e99
David Brochart <david.brochart@gmail.com>
2023-04-04 16:15:52 +0200

    Add option to sync with frontend

--- ypywidgets/reactive.py
+++ ypywidgets/reactive.py
@@ -20,10 +20,12 @@ class reactive(Reactive[ReactiveType]):
         *,
         init: bool = True,
         always_update: bool = False,
+        sync: bool = True,
     ) -> None:
+        on_set = set_attr if sync else None
         super().__init__(
             default,
             init=init,
             always_update=always_update,
-            on_set=set_attr,
+            on_set=on_set,
         )
