93a1ce259139bcb31b9a70ce6fc213fe32038b0b
David Brochart <david.brochart@gmail.com>
2023-11-30 11:27:48 +0100

    Update with pycrdt v0.5.0

--- ypywidgets/reactive.py
+++ ypywidgets/reactive.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 from typing import Callable
 
+from pycrdt import ReadTransaction
 from reacttrs.reactive import Reactive, ReactiveType
 
 
 def set_attr(obj, name, value):
-    try:
-        obj._attrs[name] = value
-    except RuntimeError:
-        # Already mutably borrowed
-        pass
+    with obj._attrs.doc.transaction() as txn:
+        # if we set the attribute, we are observing our own change: do nothing
+        # we know it's the case because callbacks provide read-only transactions
+        if not isinstance(txn, ReadTransaction):
+            obj._attrs[name] = value
 
 
 class reactive(Reactive[ReactiveType]):
