c33474a815e9c06352c6b4e9549273c99ae093cb
martinRenou <martin.renou@gmail.com>
2025-01-31 16:26:29 +0100

    Linting
    Co-authored-by: David Brochart <david.brochart@gmail.com>

--- src/ypywidgets/comm.py
+++ src/ypywidgets/comm.py
@@ -17,13 +17,13 @@ from .widget import Widget
 
 def handle_comm_opened(*args, **kwargs):
     # TODO handle comm open from front-end
-    pass
+    pass  # pragma: nocover
 
 
 def register_comm_target():
     comm_manager = comm.get_comm_manager()
     if comm_manager is not None:
-        comm_manager.register_target('ywidget', handle_comm_opened)
+        comm_manager.register_target("ywidget", handle_comm_opened)
 
 
 def create_widget_comm(
